9d187c9c8326168ca60c623a016723aed2b3720e
roman <roman.wedemeier@fatchip.de>
2018-07-26 16:49:14 +0200

    Restructure ImportProductsCronJob.php

--- Components/ImportProductsCronJob.php
+++ Components/ImportProductsCronJob.php
@@ -10,45 +10,102 @@ namespace Shopware\FatchipShopware2Afterbuy\Components;
 
 
 use Shopware\Components\Api\Resource\Article as ArticleResource;
-use Fatchip\Afterbuy\ApiClient;
 use Shopware\Components\Api\Manager as ApiManager;
 use Shopware\Models\Article\Detail as ArticleDetail;
-use Shopware\Models\Tax\Tax;
+
+use Fatchip\Afterbuy\ApiClient;
 
 
 class ImportProductsCronJob {
-    /** @var array The fields, that are objects in SW detail */
-    protected $test = [
-    ];
-
     /**
      * @return int[]
      */
     public function importProducts2Shopware() {
-        // Get SDK object
-        /** @var ApiClient $apiClient */
-        $apiClient = Shopware()->Container()->get('afterbuy_api_client');
-        /** @var ArticleResource $resource */
-        $articleResource = ApiManager::getResource('Article');
-
-
-        // Get all products from AfterbuyAPI
-        $productsResult = $apiClient->getShopProductsFromAfterbuy();
-        if ($productsResult['CallStatus'] != 'Success') {
-            // TODO: fix error handling
-            die('GetShopProducts: CallStatus: ['
-                . $productsResult['CallStatus']
-                . '] Awaited: [Success].');
-        }
-        if ($productsResult['Result']['HasMoreProducts']) {
-            // pagination on
-        } else {
-            // pagination off
-        }
-        $lastProductID = $productsResult['Result']['LastProductID'];
+        $productsResult = $this->retrieveProductsArray();
 
         $products = $productsResult['Result']['Products']['Product'];
 
+        $articles = $this->convertProduct2ArticlesArray($products);
+
+        $this->addArticles($articles);
+
+        return $productsResult;
+    }
+
+    /**
+     * @param array $product - Array with product data, as it comes from the
+     *                       Afterbuy API.
+     *
+     * @return array
+     */
+    protected function createArticleArray(array $product) {
+        $article = [];
+
+        $article['name'] = $product['Name'];
+        $article['description'] = $product['ShortDescription'];
+        $article['descriptionLong'] = $product['Description'];
+        $article['shippingtime'] = $product['DeliveryTime'];
+        $article['tax'] = $product['TaxRate'];
+        $article['keywords'] = $product['Keywords'];
+        $article['changed'] = $product['ModDate'];
+
+        // TODO: what to map here?
+        $article['datum'] = $product[''];
+        $article['active'] = 1;
+        $article['pseudosales'] = 0;
+        $article['topseller'] = $product[''];
+        $article['metaTitle'] = $product[''];
+        $article['pricegroupID'] = $product[''];
+        $article['pricegroupActive'] = 0;
+        $article['filtergroupID'] = $product[''];
+        $article['laststock'] = $product['Discontinued'] & $product['Stock'];
+        $article['crossbundleloock'] = $product[''];
+        $article['notification'] = 0;
+        $article['template'] = '';
+        $article['mode'] = 0;
+
+        return $article;
+    }
+
+    /**
+     * @param array $product - Array with product data, as it comes from the
+     *                       Afterbuy API.
+     *
+     * @return array
+     */
+    protected function createDetailArray(array $product) {
+        $detail = [];
+
+        $detail['number'] = $product['Anr'];
+        $detail['supplierNumber'] = $product['ManufacturerPartNumber'];
+        $detail['shippingTime'] = $product['DeliveryTime'];
+        $detail['laststock'] = $product['Discontinued'] & $product['Stock'];
+
+        return $detail;
+    }
+
+    /**
+     * Adds the given detail to the given article. When detail is mainDetail,
+     * the detail is set article's mainDetail field. Otherwise the detail is
+     * added to the details array.
+     *
+     * @param bool  $isMainDetail
+     * @param array $article
+     * @param array $detail
+     */
+    protected function addDetailToArticle($isMainDetail, &$article, $detail) {
+        // mainDetail?
+        if ($isMainDetail) {
+            $article['mainDetail'] = $detail;
+        } else {
+            array_push(
+                $article['variants'],
+                $detail
+            );
+        }
+    }
+
+    protected function convertProduct2ArticlesArray($products) {
         $articles = [];
         $details = [];
         $mainDetailsMap = [];
@@ -66,23 +123,34 @@ class ImportProductsCronJob {
                     $currentParentProduct = $product;
                     $currentParentProductID = $productID;
 
-                    $variantSets[$currentParentProductID] = $currentParentProduct;
+                    $variantSets[$currentParentProductID]
+                        = $currentParentProduct;
 
-                    $articles[$currentParentProductID] = $this->createArticleArray($currentParentProduct);
-                    $articles[$currentParentProductID]['details'] = [];
+                    $articles[$currentParentProductID]
+                        = $this->createArticleArray($currentParentProduct);
+                    $articles[$currentParentProductID]['variants'] = [];
 
                     // foreach variant set product
-                    foreach ($currentParentProduct['BaseProducts']['BaseProduct'] as $currentChildProduct) {
-                        $currentChildProductID = $currentChildProduct['BaseProductID'];
+                    foreach (
+                        $currentParentProduct['BaseProducts']['BaseProduct'] as
+                        $currentChildProduct
+                    ) {
+                        $currentChildProductID
+                            = $currentChildProduct['BaseProductID'];
 
                         // mainDetail?
-                        if ($currentChildProduct['BaseProductsRelationData']['DefaultProduct'] == -1) {
-                            $mainDetailsMap[$currentParentProductID] = $currentChildProductID;
+                        if ($currentChildProduct['BaseProductsRelationData']['DefaultProduct']
+                            == -1
+                        ) {
+                            $mainDetailsMap[$currentParentProductID]
+                                = $currentChildProductID;
                         }
 
                         // detail already processed?
                         if (isset($details[$currentChildProductID])) {
-                            $isMainDetail = $mainDetailsMap[$currentParentProductID] == $currentChildProductID;
+                            $isMainDetail
+                                = $mainDetailsMap[$currentParentProductID]
+                                == $currentChildProductID;
                             $this->addDetailToArticle(
                                 $isMainDetail,
                                 $articles[$currentParentProductID],
@@ -90,16 +158,19 @@ class ImportProductsCronJob {
                             );
                         }
                     }
-                // variantSet childObject
+                    // variantSet childObject
                 } else {
                     $currentChildProductID = $productID;
-                    $parentProductID = $product['BaseProducts']['BaseProduct']['BaseProductID'];
+                    $parentProductID
+                        = $product['BaseProducts']['BaseProduct']['BaseProductID'];
 
-                    $details[$currentChildProductID] = $this->createDetailArray($product);
+                    $details[$currentChildProductID]
+                        = $this->createDetailArray($product);
 
                     // variant set already processed?
                     if (isset($articles[$parentProductID])) {
-                        $isMainDetail = $mainDetailsMap[$parentProductID] == $currentChildProductID;
+                        $isMainDetail = $mainDetailsMap[$parentProductID]
+                            == $currentChildProductID;
                         $this->addDetailToArticle(
                             $isMainDetail,
                             $articles[$parentProductID],
@@ -120,141 +191,68 @@ class ImportProductsCronJob {
                     $details[$productID]
                 );
             }
-
         }
-
-        foreach ($articles as $article) {
-            $repo = Shopware()
-                ->Models()
-                ->getRepository('Shopware\Models\Article\Detail');
-            /** @var ArticleDetail[] $a */
-            $mainDetail_AB = $article['mainDetail'];
-            $mainDetail_SW = $repo->findBy(['number' => $mainDetail_AB['number']]);
-
-            // if article exists in db
-            if ($mainDetail_SW) {
-                foreach ($mainDetail_AB as $key => $value) {
-                    $setter = 'set' . ucfirst($key);
-                    $getter = 'get' . ucfirst($key);
-
-                    // getter exists in Shopware detail?
-                    if (method_exists($mainDetail_SW[0], $getter)) {
-                        $this->updateValue(
-                            $mainDetail_SW[0],
-                            $getter,
-                            $setter,
-                            $value
-                        );
-                    }
-                }
-
-                foreach ($mainDetail_AB as $key => $value) {
-
-                }
-                // if article has changed
-                    // update it
-                // else do nothing
-            }
-            // else create it
-            else {
-                $articleResource->create($article);
-            }
-        }
-
-        return $productsResult;
-    }
-
-    protected function updateValue($mainDetail_SW, $getter, $setter, $value_AB) {
-        // compare value sw - ab
-        // setter exists in Shopware detail?
-        if (method_exists($mainDetail_SW, $setter)) {
-            // overwrite it in Shopware
-            $mainDetail_SW->$setter($value_AB);
-        }
-    }
-
-    /**
-     * @param array $product - Array with product data, as it comes from the
-     *                       Afterbuy API.
-     *
-     * @return array
-     */
-    protected function createArticleArray(array $product) {
-        $articles = [];
-
-        /** @var Tax $taxObject */
-        $taxObject = Shopware()
-            ->Models()
-            ->getRepository('Shopware\Models\Tax\Tax')
-            ->findOneBy(['tax' => $product['TaxRate']]);
-
-        if ( ! $taxObject) {
-            // TODO: test, that the new tax does not break the shop. Do we need a country relation?
-            $taxObject = new Tax();
-            $taxObject->setTax($product['TaxRate']);
-            $taxObject->setName($product['TaxRate'] . '%');
-        }
-
-        $articles['name'] = $product['Name'];
-        $articles['description'] = $product['ShortDescription'];
-        $articles['descriptionLong'] = $product['Description'];
-        $articles['shippingtime'] = $product['DeliveryTime'];
-        $articles['tax'] = $product['TaxRate'];
-        $articles['keywords'] = $product['Keywords'];
-        $articles['changed'] = $product['ModDate'];
-
-        // TODO: what to map here?
-        $articles['datum'] = $product[''];
-        $articles['active'] = 1;
-        $articles['pseudosales'] = 0;
-        $articles['topseller'] = $product[''];
-        $articles['metaTitle'] = $product[''];
-        $articles['pricegroupID'] = $product[''];
-        $articles['pricegroupActive'] = 0;
-        $articles['filtergroupID'] = $product[''];
-        $articles['laststock'] = $product['Discontinued'] & $product['Stock'];
-        $articles['crossbundleloock'] = $product[''];
-        $articles['notification'] = 0;
-        $articles['template'] = '';
-        $articles['mode'] = 0;
 
         return $articles;
     }
 
-    /**
-     * @param array $product - Array with product data, as it comes from the
-     *                       Afterbuy API.
-     *
-     * @return array
-     */
-    protected function createDetailArray(array $product) {
-        $detail = [];
+    protected function addArticles($articles) {
+        /** @var ArticleResource $resource */
+        $articleResource = ApiManager::getResource('Article');
 
-        $detail['number'] = $product['Anr'];
-        $detail['supplierNumber'] = $product['ManufacturerPartNumber'];
-        $detail['shippingTime'] = $product['DeliveryTime'];
+        foreach ($articles as $articleArray) {
+            $modelManager = Shopware()->Models();
+            $repo = $modelManager->getRepository(
+                'Shopware\Models\Article\Detail'
+            );
+            /** @var ArticleDetail[] $mainDetail_AB */
+            $mainDetail_AB = $articleArray['mainDetail'];
+            /** @var ArticleDetail[] $mainDetail_SW */
+            $mainDetail_SW = $repo->findBy(
+                ['number' => $mainDetail_AB['number']]
+            );
 
-        return $detail;
+            // article exists in db?
+            if ($mainDetail_SW) {
+                // if article has changed
+                // update it
+                // else do nothing
+                //
+                // SW handles updates itself - Yay
+                $articleResource->update(
+                    $mainDetail_SW[0]->getArticleId(),
+                    $articleArray
+                );
+            } // else create it
+            else {
+                $articleResource->create($articleArray);
+            }
+        }
     }
 
     /**
-     * Adds the given detail to the given article. When detail is mainDetail,
-     * the detail is set article's mainDetail field. Otherwise the detail is
-     * added to the details array.
-     *
-     * @param array $isMainDetail
-     * @param array $article
-     * @param array $detail
+     * @return array
      */
-    protected function addDetailToArticle($isMainDetail, &$article, $detail) {
-        // mainDetail?
-        if ($isMainDetail) {
-            $article['mainDetail'] = $detail;
-        } else {
-            array_push(
-                $article['details'],
-                $detail
-            );
+    protected function retrieveProductsArray() {
+// Get SDK object
+        /** @var ApiClient $apiClient */
+        $apiClient = Shopware()->Container()->get('afterbuy_api_client');
+
+
+        // Get all products from AfterbuyAPI
+        $productsResult = $apiClient->getShopProductsFromAfterbuy();
+        if ($productsResult['CallStatus'] != 'Success') {
+            // TODO: fix error handling
+            die('GetShopProducts: CallStatus: ['
+                . $productsResult['CallStatus']
+                . '] Awaited: [Success].');
         }
+        if ($productsResult['Result']['HasMoreProducts']) {
+            // pagination on
+        } else {
+            // pagination off
+        }
+
+        return $productsResult;
     }
 }

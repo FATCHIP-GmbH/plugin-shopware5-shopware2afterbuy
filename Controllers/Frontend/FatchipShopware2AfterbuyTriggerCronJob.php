313ab9b3a5a4bec2421327e75f8eec46c4f3ed49
roman <roman.wedemeier@fatchip.de>
2018-08-15 17:52:47 +0200

    0040158: WIP add variant article per API to compare with manual created one

--- Controllers/Frontend/FatchipShopware2AfterbuyTriggerCronJob.php
+++ Controllers/Frontend/FatchipShopware2AfterbuyTriggerCronJob.php
@@ -23,4 +23,103 @@ class Shopware_Controllers_Frontend_FatchipShopware2AfterbuyTriggerCronJob exten
 
         var_dump($xml);
     }
+
+    public function testAction() {
+        $newArticle = array(
+            'configuratorSet' => array(
+                'groups' => array(
+                    array(
+                        'name' => 'Farbe',
+                        'options' => array(
+                            array('name' => 'Rot'),
+                            array('name' => 'Blau'),
+                            array('name' => 'Weiß'),
+                        )
+                    ),
+                )
+            ),
+            'name'       => 'Jacke',
+            'description' => 'Meta-Kurzbeschreibung',
+            'descriptionLong' => '<p>asdf</p>p>',
+            'shippingtime' => '',
+            'taxId'      => 1,
+            'keywords'         => 'Meta-Keywords',
+            'changed'          => '2018-08-15 16:23:30',
+            'active'           => 1,
+            'pseudoSales'      => 0,
+            'highlight'        => false,
+            'metaTitle'        => 'Meta-Titel',
+            'lastStock'        => '',
+            'notification'     => false,
+            'template'         => '',
+            'supplier'         => '',
+            'availableFrom'    => null,
+            'availableTo'      => null,
+            'priceGroup'       => null,
+            'pricegroupActive' => false,
+            'propertyGroup'    => null,
+            'crossBundleLook'  => false,
+            'BaseProductFlag'  => '',
+
+            // TODO: what to map here?
+
+            // could not find field in AB API
+            'added'            => '2018-08-15',
+            // not sure what kind of mode is meant
+            'mode'             => 0,
+            'supplierId' => 1,
+            'pricegroupID' => 1,
+            'variants' => array(
+                array(
+                    'isMain' => true,
+                    'number' => 'turn',
+                    'inStock' => 15,
+                    'additionaltext' => 'L / Black',
+                    'configuratorOptions' => array(
+                        array('group' => 'Size', 'option' => 'L'),
+                        array('group' => 'Color', 'option' => 'Black'),
+                    ),
+                    'prices' => array(
+                        array(
+                            'customerGroupKey' => 'EK',
+                            'price' => 1999,
+                        ),
+                    )
+                ),
+                array(
+                    'isMain' => false,
+                    'number' => 'turn.1',
+                    'inStock' => 15,
+                    'additionaltext' => 'S / Black',
+                    'configuratorOptions' => array(
+                        array('group' => 'Size', 'option' => 'S'),
+                        array('group' => 'Color', 'option' => 'Black'),
+                    ),
+                    'prices' => array(
+                        array(
+                            'customerGroupKey' => 'EK',
+                            'price' => 999,
+                        ),
+                    )
+                ),
+                array(
+                    'isMain' => false,
+                    'number' => 'turn.2',
+                    'inStock' => 15,
+                    'additionaltext' => 'S / Red',
+                    'configuratorOptions' => array(
+                        array('group' => 'Size', 'option' => 'S'),
+                        array('group' => 'Color', 'option' => 'Red'),
+                    ),
+                    'prices' => array(
+                        array(
+                            'customerGroupKey' => 'EK',
+                            'price' => 999,
+                        ),
+                    )
+                )
+            )
+        );
+
+        $client->put('articles/193', $updateArticle);}
 }

77bc9fe1ec04a743a4f85e266531ef6eeda4e4a3
roman <roman.wedemeier@fatchip.de>
2019-03-04 12:08:29 +0100

    0048644: Add ShopwareResetHelper

---
+++ Services/Helper/ShopwareResetHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace viaebShopwareAfterbuy\Services\Helper;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+class ShopwareResetHelper extends AbstractHelper
+{
+    protected $entities;
+
+    public function initHelper($entities)
+    {
+        $this->entities = $entities;
+    }
+
+    public function resetShopConnection()
+    {
+        $result = [
+            'msg' => 'success',
+            'data' => [],
+        ];
+
+        foreach ($this->entities as $entity) {
+            if ($this->resetShopConnectionEntity($entity) !== 'success') {
+                $result['msg'] = 'failure';
+                $result['data'][] = $entity;
+            }
+        }
+
+        return $result;
+    }
+
+    private function resetShopConnectionEntity($entity)
+    {
+        $prefix = 'afterbuy';
+
+        /** @var ClassMetadata $metadata */
+        $metadata = $this->entityManager->getClassMetadata($entity);
+
+        $builder = $this->entityManager->createQueryBuilder();
+        $builder->update($entity, 'a');
+
+        foreach ($metadata->fieldMappings as $name => $column) {
+            // column does not start with afterbuy?
+            if (substr($name, 0, strlen($prefix)) !== $prefix) {
+                continue;
+            }
+
+            $builder->set('a.' . $name, ':null');
+        }
+
+        $builder
+            ->setParameter('null', null)
+            ->getQuery()
+            ->execute();
+
+        die();
+    }
+}

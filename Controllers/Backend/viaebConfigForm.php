4b311c12276d83045290814109e6bff99bd53937
stephan.altmann <stephan.altmann@fatchip.de>
2019-03-18 14:47:03 +0100

    added action for saving config

--- Controllers/Backend/viaebConfigForm.php
+++ Controllers/Backend/viaebConfigForm.php
@@ -3,6 +3,19 @@
 
 class Shopware_Controllers_Backend_viaebConfigForm extends Shopware_Controllers_Backend_ExtJs
 {
+    /** @var \Shopware\Components\ConfigWriter $configWriter */
+    protected $configWriter;
+
+    protected $pluginName;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->configWriter = $this->container->get('config_writer');
+        $this->pluginName = $this->container->getParameter('');
+    }
+
     public function indexAction()
     {
     }
@@ -15,5 +28,15 @@ class Shopware_Controllers_Backend_viaebConfigForm extends Shopware_Controllers_
             'data' => $result['data'],
             'total' => count($result['data']),
         ]);
+    }
+
+    public function saveConnectionConfigAction() {
+        $request = $this->Request();
+
+
+        $this->configWriter->save('partnerId', $_REQUEST['partnerId'], $this->pluginName);
+        //TODO: save
+
+        //TODO: clear config cache
     }
 }

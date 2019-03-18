0f5d4fa7d5bedd0204fe03d85238e8d3ebdbd8fe
stephan.altmann <stephan.altmann@fatchip.de>
2019-03-18 15:56:59 +0100

    added configuration test

--- Controllers/Backend/viaebConfigForm.php
+++ Controllers/Backend/viaebConfigForm.php
@@ -34,6 +34,48 @@ class Shopware_Controllers_Backend_viaebConfigForm extends Shopware_Controllers_
         ]);
     }
 
+    public function testConnectionConfigAction() {
+        $testService = $this->container->get('viaeb_shopware_afterbuy.services.read_data.external.connection_test_service');
+        $response = $testService->test(array(
+                'partnerId' => $_REQUEST['partnerId'],
+                'userName' => $_REQUEST['userName'],
+                'partnerPassword' => $_REQUEST['partnerPassword'],
+                'userPassword' => $_REQUEST['userPassword']
+            )
+        );
+
+        if(array_key_exists('AfterbuyTimeStamp', $response['Result'])) {
+            $this->view->assign([
+                'success' => true,
+            ]);
+
+            return;
+        }
+
+        if(array_key_exists('ErrorList', $response['Result']) && array_key_exists('Error', $response['Result']['ErrorList'])) {
+
+            if(array_key_exists('ErrorDescription', $response['Result']['ErrorList']['Error'])) {
+                $error = $response['Result']['ErrorList']['Error']['ErrorDescription'];
+            }
+            else {
+                $error = '';
+
+                foreach($response['Result']['ErrorList']['Error'] as $element) {
+                    $error .= $element['ErrorDescription'];
+                }
+            }
+
+            $this->view->assign([
+                'success' => false,
+                'data' => [
+                    'error' => $error,
+                ],
+            ]);
+
+            return;
+        }
+    }
+
     public function saveConnectionConfigAction() {
 
         $this->View()->assign([

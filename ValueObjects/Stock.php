4e828e26567311bf08b3ec3c04c2a9683513c90c
andrefatchip <andre.herrmann@fatchip.de>
2020-01-09 23:19:07 +0100

    0057389: Removed PHP 7.3 only feature

--- ValueObjects/Stock.php
+++ ValueObjects/Stock.php
@@ -24,7 +24,7 @@ class Stock extends AbstractValueObject
     /**
      * @return string
      */
-    public function getIdentifyer(): string
+    public function getIdentifyer()
     {
         return $this->identifyer;
     }

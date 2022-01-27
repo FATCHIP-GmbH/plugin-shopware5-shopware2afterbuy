9dec688326fce63302bbbe9f248bc216ad134606
Stefan Mueller <stefstar@stefstar.de>
2022-01-27 15:03:57 +0100

    0082811: prevent non-exportable orders from clogging the queue

--- Services/Helper/ShopwareOrderHelper.php
+++ Services/Helper/ShopwareOrderHelper.php
@@ -388,9 +388,10 @@ class ShopwareOrderHelper extends AbstractHelper
                 ->setParameters(array('minOrderDate' => $minOrderDate));
         }
 
+        $query->orderBy('orders.id', 'DESC');
         $orders = $query
             ->getQuery()
-            ->setMaxResults(200)
+            ->setMaxResults(1000)
             ->getResult();
 
         // die(var_dump($config));
